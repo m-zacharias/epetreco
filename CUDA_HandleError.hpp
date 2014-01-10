6ef94176454e0700c01ffe613ef083dca93bff16
malte <malte@a6ced6be-26ad-11e3-96f0-b1ac50a5cd04>
2014-01-10 17:35:40 +0000

    CUDA_HanleError.hpp:     added.  Handling of CUDA runtime errors.

---
+++ CUDA_HandleError.hpp
@@ -0,0 +1,14 @@
+#ifndef CUDA_HANDLE_ERROR
+#define CUDA_HANDLE_ERROR
+
+void HandleError( cudaError_t err, const char * file, int line )
+{
+  if(err != cudaSuccess)
+    {
+    printf("%s(%d): error: %s\n", file, line, cudaGetErrorString( err ) );
+    exit( EXIT_FAILURE );
+    }
+}
+#define HANDLE_ERROR( err ) {HandleError( err, __FILE__, __LINE__ );};
+
+#endif  // #ifndef CUDA_HANDLE_ERROR
