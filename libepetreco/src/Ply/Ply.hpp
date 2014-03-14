4331bb9ee34370bd72159b779c0f22f04620d220
malte <malte@a6ced6be-26ad-11e3-96f0-b1ac50a5cd04>
2014-03-14 11:25:49 +0000

    Added directory Ply/ in src/, moved Ply classes and associated test .cpps there. Added header file Ply.hpp which pulls all Ply classes' headers. Adapted Makefile.

---
+++ libepetreco/src/Ply/Ply.hpp
@@ -0,0 +1,11 @@
+#ifndef PLY_HPP
+#define PLY_HPP
+
+#include "PlyGeometry.hpp"
+#include "PlyBox.hpp"
+#include "PlyLine.hpp"
+#include "PlyGrid.hpp"
+#include "PlyRectangle.hpp"
+#include "PlyWriter.hpp"
+
+#endif  // #ifndef PLY_HPP
