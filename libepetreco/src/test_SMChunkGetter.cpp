e4602e694b8e52bbc7319db331d2ff049dcde97a
malte <malte@a6ced6be-26ad-11e3-96f0-b1ac50a5cd04>
2014-03-25 15:56:30 +0000

    Adapted test_SMChunkGetter.cpp : Now utilizes FileTalk.hpp from the lasst commit

--- libepetreco/src/test_SMChunkGetter.cpp
+++ libepetreco/src/test_SMChunkGetter.cpp
@@ -3,26 +3,23 @@
 #include "DevelSetup.hpp"
 #include "DevelSMChunk.hpp"
 #include "Ply.hpp"
+#include "FileTalk.hpp"
 
 int main()
 {
-  std::cout << "DevelSetup setup(5140980);" << std::endl;
+  SAYLINE(__LINE__+1);
   DevelSetup setup(5140980);
 
-  std::cout
-  << "DevelGrid  grid(DevelGrid::Vertex_t(0,0,0), DevelGrid::Vertex_t(0.1,0.1,0.1), 1,1,1);"
-  << std::endl;
-  DevelGrid  grid(DevelGrid::Vertex_t(0,0,0), DevelGrid::Vertex_t(0.1,0.1,0.1), 1,1,1);
+  SAYLINE(__LINE__+1);SAYLINE(__LINE__+2);
+  DevelGrid  grid(DevelGrid::Vertex_t(0,0,0),
+                  DevelGrid::Vertex_t(0.1,0.1,0.1), 1,1,1);
   
-  std::cout
-  << "DevelSMChunk sm(setup, grid);"
-  << std::endl;
+  SAYLINE(__LINE__+1);
   DevelSMChunk sm(setup, grid);
 
-  std::cout
-  << "SMChunkGetter<DevelSetup, DevelGrid, DevelSMChunk>()(setup, grid, 0, 10000, sm, 1);"
-  << std::endl;
-  SMChunkGetter<DevelSetup, DevelGrid, DevelSMChunk>()(setup, grid, 0, 10000, sm, 1);
+  SAYLINE(__LINE__+1);SAYLINE(__LINE__+2);
+  SMChunkGetter<DevelSetup, DevelGrid, DevelSMChunk>()
+        (setup, grid, 0, 10000, sm, 1);
   
   return 0;
 }
