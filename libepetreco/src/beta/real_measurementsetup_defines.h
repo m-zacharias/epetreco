e6e8f65018fddd20f1922f7553ce2dcda55ff707
malte <malte@a6ced6be-26ad-11e3-96f0-b1ac50a5cd04>
2014-07-07 13:27:32 +0000

    beta/voxelgrid10_defines.h, beta/real_measurementsetup_defines.h :   units to [m], to be consistent with other defines files.

--- libepetreco/src/beta/real_measurementsetup_defines.h
+++ libepetreco/src/beta/real_measurementsetup_defines.h
@@ -6,12 +6,12 @@
 #define N1Z 13      // 2nd detector's number of segments in z
 #define N1Y 13      // 2nd detector's number of segments in y
 #define NA  180     // number of angular positions
-#define DA  2.      // angular step
-#define POS0X -457. // position of 1st detector's center in x [mm]
-#define POS1X  457. // position of 2nd detector's center in x [mm]
-#define SEGX 20.    // x edge length of one detector segment [mm]
-#define SEGY 4.     // y edge length of one detector segment [mm]
-#define SEGZ 4.     // z edge length of one detector segment [mm]
+#define DA     2.     // angular step
+#define POS0X -0.457  // position of 1st detector's center in x [m]
+#define POS1X  0.457  // position of 2nd detector's center in x [m]
+#define SEGX   0.020  // x edge length of one detector segment [m]
+#define SEGY   0.004  // y edge length of one detector segment [m]
+#define SEGZ   0.004  // z edge length of one detector segment [m]
 #define NCHANNELS NA*N0Z*N0Y*N1Z*N1Y
 
 #endif  // #ifndef MEASUREMENTSETUP_DEFINES
--- libepetreco/src/beta/voxelgrid10_defines.h
+++ libepetreco/src/beta/voxelgrid10_defines.h
@@ -4,12 +4,12 @@
 #define GRIDNX 10   // x dimension of voxel grid
 #define GRIDNY 10   // y dimension of voxel grid
 #define GRIDNZ 10   // z dimension od voxel grid
-#define GRIDOX -350.// x origin of voxel grid [mm]
-#define GRIDOY -350.// y origin of voxel grid [mm]
-#define GRIDOZ -350.// z origin of voxel grid [mm]
-#define GRIDDX 70.  // x edge length of one voxel [mm]
-#define GRIDDY 70.  // y edge length of one voxel [mm]
-#define GRIDDZ 70.  // z edge length of one voxel [mm]
+#define GRIDOX -0.350 // x origin of voxel grid [m]
+#define GRIDOY -0.350 // y origin of voxel grid [m]
+#define GRIDOZ -0.350 // z origin of voxel grid [m]
+#define GRIDDX  0.070 // x edge length of one voxel [m]
+#define GRIDDY  0.070 // y edge length of one voxel [m]
+#define GRIDDZ  0.070 // z edge length of one voxel [m]
 #define VGRIDSIZE GRIDNX*GRIDNY*GRIDNZ
 
 #endif  // #define VOXELGRID_DEFINES
