9779e8a64850f306ba82600365b04d2e0ab1934f
malte <malte@a6ced6be-26ad-11e3-96f0-b1ac50a5cd04>
2014-09-22 13:26:09 +0000

    A    trunk/libepetreco/src/tests/test_CudaMatrix.cu

---
+++ libepetreco/src/tests/test_CudaMatrix.cu
@@ -0,0 +1,29 @@
+#include "CudaMatrix.hpp"
+#include "CudaTransform.hpp"
+#include <cassert>
+
+#define NCOLS 1000
+#define NROWS 100000
+
+int main()
+{
+  typedef float val_t;
+
+  CudaMatrix<val_t, val_t> A(NCOLS, NROWS);
+  
+  for(int colId=0; colId<NCOLS; colId++)
+    for(int rowId=0; rowId<NROWS; rowId++)
+      A.set(colId, rowId, 0.);
+
+  for(int colId=0; colId<NCOLS; colId++)
+  {
+    for(int rowId=0; rowId<NROWS; rowId++)
+    {
+      val_t elem = A.get(colId, rowId);
+      assert(!isnan(elem));
+      assert(!isinf(elem));
+    }
+  }
+
+  return 0;
+}
