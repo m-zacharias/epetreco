612fed48177f35f62c3822607ba911764841357d
malte <malte@a6ced6be-26ad-11e3-96f0-b1ac50a5cd04>
2015-02-18 09:35:42 +0000

    M    libepetreco/src/distancePointLine.h   Bug fix in distance: Calculate sine of angle using the cross product instead   of the (instable) detour using cosine and arcus cosine.

--- libepetreco/src/distancePointLine.h
+++ libepetreco/src/distancePointLine.h
@@ -36,6 +36,19 @@ inline T scalarProduct( T const * const a, T const * const b )
     return a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
 }
 
+/** @brief Calculate the cross product of two vectors.
+ * @param result A 3 component array.
+ * @param a A 3 component array (= "vector"). One factor.
+ * @param b A 3 component array (= "vector"). One factor. */
+template<typename T>
+__host__ __device__
+inline void crossProduct( T * const result, T const * const a, T const * const b )
+{
+  for(int dim=0; dim<3; dim++) {
+    result[dim] = a[(dim+1)%3]*b[(dim+2)%3] - a[(dim+2)%3]*b[(dim+1)%3];
+  }
+}
+
 /**
  * Calculate the minimum distance between a point and a line.
  * @param a A 3 component array (= "vector"). Position vector of one point one the line.
@@ -50,29 +63,13 @@ T distance( T const * const a, T const * const b,
 {
   T ap[3];
   T ab[3];
+  T c[3];
   for(int dim=0; dim<3; dim++) {
     ap[dim] = p[dim]-a[dim];
     ab[dim] = b[dim]-a[dim];
   }
-
-  T sp = scalarProduct(ab,ap);
-  T abs_ab = absolute(ab);
-  T abs_ap = absolute(ap);
-
-  T cos_alpha = scalarProduct(ab,ap)/(absolute(ab)*absolute(ap));
-  
-  if(cos_alpha<-1.) {
-    printf("cos_alpha not in range! cos_alpha+1: %.20e\n", cos_alpha+1);
-    // use: cos_alpha = -1.  =>  sin(acos(cos_alpha)) = 0.
-    return 0.;
-  }
-  if(cos_alpha>1.) {  
-    printf("cos_alpha not in range! cos_alpha-1: %.20e\n", cos_alpha-1);
-    // use: cos_alpha = +1.  =>  sin(acos(cos_alpha)) = 0.
-    return 0.;
-  }
-  return absolute(ap)*
-            sin(acos(scalarProduct(ab,ap)/absolute(ab)/absolute(ap)));
+  crossProduct(c, ab, ap);
+  return absolute(c) / absolute(ab);
 }
 
 #endif	/* DISTANCEPOINTLINE_H */
