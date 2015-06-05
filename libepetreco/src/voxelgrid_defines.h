1934edfa91ccbab6fa09071fb66329d5abf7cfd6
malte <malte@a6ced6be-26ad-11e3-96f0-b1ac50a5cd04>
2015-06-05 13:33:42 +0000

    Put all voxelgrid definitions in voxelgrid_defines.h thus making the individual voxelgrid definition files obsolete.
    Renamed libepetreco/mpireco_wrapper.bash to
    libepetreco/onlyOneDeviceVisible_wrapper.bash as this wrapper script is a
    general one rather than that it's limited to be used with reco.out only.

--- libepetreco/src/voxelgrid_defines.h
+++ libepetreco/src/voxelgrid_defines.h
@@ -12,24 +12,89 @@ extern "C" {
 
   
 #ifdef GRID64
-#include "voxelgrid64_defines.h"
+
+#define GRIDNX 64                       /** x dimension of voxel grid */
+#define GRIDNY 64                       /** y dimension of voxel grid */
+#define GRIDNZ 64                       /** z dimension od voxel grid */
+#define GRIDOX -0.030                   /** x origin of voxel grid [m] */
+#define GRIDOY -0.030                   /** y origin of voxel grid [m] */
+#define GRIDOZ -0.030                   /** z origin of voxel grid [m] */
+#define GRIDDX  0.0009375               /** x edge length of one voxel [m] */
+#define GRIDDY  0.0009375               /** y edge length of one voxel [m] */
+#define GRIDDZ  0.0009375               /** z edge length of one voxel [m] */
+#define VGRIDSIZE (GRIDNX*GRIDNY*GRIDNZ)/** Number of voxel in the voxel grid */
 
 #else
 #ifdef GRID52
-#include "voxelgrid52_defines.h"
+  
+#define GRIDNX 52                       /** x dimension of voxel grid */
+#define GRIDNY 52                       /** y dimension of voxel grid */
+#define GRIDNZ 52                       /** z dimension od voxel grid */
+#define GRIDOX -0.026                   /** x origin of voxel grid [m] */
+#define GRIDOY -0.026                   /** y origin of voxel grid [m] */
+#define GRIDOZ -0.026                   /** z origin of voxel grid [m] */
+#define GRIDDX  0.001                   /** x edge length of one voxel [m] */
+#define GRIDDY  0.001                   /** y edge length of one voxel [m] */
+#define GRIDDZ  0.001                   /** z edge length of one voxel [m] */
+#define VGRIDSIZE (GRIDNX*GRIDNY*GRIDNZ)/** Number of voxel in the voxel grid */
   
 #else
 #ifdef GRID32
-#include "voxelgrid32_defines.h"
+  
+#define GRIDNX 32                       /** x dimension of voxel grid */
+#define GRIDNY 32                       /** y dimension of voxel grid */
+#define GRIDNZ 32                       /** z dimension od voxel grid */
+#define GRIDOX -0.030                   /** x origin of voxel grid [m] */
+#define GRIDOY -0.030                   /** y origin of voxel grid [m] */
+#define GRIDOZ -0.030                   /** z origin of voxel grid [m] */
+#define GRIDDX  0.001875                /** x edge length of one voxel [m] */
+#define GRIDDY  0.001875                /** y edge length of one voxel [m] */
+#define GRIDDZ  0.001875                /** z edge length of one voxel [m] */
+#define VGRIDSIZE (GRIDNX*GRIDNY*GRIDNZ)/** Number of voxel in the voxel grid */
   
 #else
 #ifdef GRID20
-#include "voxelgrid20_defines.h"
+
+#define GRIDNX 20                       /** x dimension of voxel grid */
+#define GRIDNY 20                       /** y dimension of voxel grid */
+#define GRIDNZ 20                       /** z dimension od voxel grid */
+#define GRIDOX -0.030                   /** x origin of voxel grid [m] */
+#define GRIDOY -0.030                   /** y origin of voxel grid [m] */
+#define GRIDOZ -0.030                   /** z origin of voxel grid [m] */
+#define GRIDDX  0.003                   /** x edge length of one voxel [m] */
+#define GRIDDY  0.003                   /** y edge length of one voxel [m] */
+#define GRIDDZ  0.003                   /** z edge length of one voxel [m] */
+#define VGRIDSIZE (GRIDNX*GRIDNY*GRIDNZ)/** Number of voxel in the voxel grid */
 
 #else
 #ifdef GRID10
-#include "voxelgrid10_defines.h"
+  
+#define GRIDNX 10                       /** x dimension of voxel grid */
+#define GRIDNY 10                       /** y dimension of voxel grid */
+#define GRIDNZ 10                       /** z dimension od voxel grid */
+#define GRIDOX -0.350                   /** x origin of voxel grid [m] */
+#define GRIDOY -0.350                   /** y origin of voxel grid [m] */
+#define GRIDOZ -0.350                   /** z origin of voxel grid [m] */
+#define GRIDDX  0.070                   /** x edge length of one voxel [m] */
+#define GRIDDY  0.070                   /** y edge length of one voxel [m] */
+#define GRIDDZ  0.070                   /** z edge length of one voxel [m] */
+#define VGRIDSIZE (GRIDNX*GRIDNY*GRIDNZ)/** Number of voxel in the voxel grid */
 
+#else
+#ifdef GRID128
+
+#define GRIDNX 128                      /** x dimension of voxel grid */
+#define GRIDNY 128                      /** y dimension of voxel grid */
+#define GRIDNZ 128                      /** z dimension od voxel grid */
+#define GRIDOX -0.030                   /** x origin of voxel grid [m] */
+#define GRIDOY -0.030                   /** y origin of voxel grid [m] */
+#define GRIDOZ -0.030                   /** z origin of voxel grid [m] */
+#define GRIDDX  0.00046875              /** x edge length of one voxel [m] */
+#define GRIDDY  0.00046875              /** y edge length of one voxel [m] */
+#define GRIDDZ  0.00046875              /** z edge length of one voxel [m] */
+#define VGRIDSIZE (GRIDNX*GRIDNY*GRIDNZ)/** Number of voxel in the voxel grid */
+
+#endif /* GRID128 */
 #endif /* GRID10 */
 #endif /* GRID20 */
 #endif /* GRID32 */
